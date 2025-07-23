6de69a5e66209fcd7fca9b3d447077a5663bcc5b
ext-aunge <ext-aunge@diasteknoloji.com>
2025-07-23 09:07:29 +0300

    Add restored file functionality

--- src/FileStorage.ConsoleApp/Services/IFileProcessor.cs
+++ src/FileStorage.ConsoleApp/Services/IFileProcessor.cs
@@ -3,4 +3,6 @@ namespace FileStorage.ConsoleApp.Services;
 public interface IFileProcessor
 {
     Task<Guid> ProcessFileAsync(string filePath);
+
+    Task<string> RestoreFile(Guid fileId, string outputDirectory);
 }
